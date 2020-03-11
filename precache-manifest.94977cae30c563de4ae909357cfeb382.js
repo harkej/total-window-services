02fdbf3e62d1d7bfc092ec84ac39fb11aeb885b1
Harsh Kejriwal <hkejriwal@deloitte.com>
2020-03-12 04:15:44 +0530

    Updates

---
+++ precache-manifest.94977cae30c563de4ae909357cfeb382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985d1471c9c643bb9455bd3ce0d160f4",
+    "url": "/total-window-services/index.html"
+  },
+  {
+    "revision": "a6db0023dc28abad21aa",
+    "url": "/total-window-services/static/css/main.d25ed0c2.chunk.css"
+  },
+  {
+    "revision": "a3951754e9779097a7ff",
+    "url": "/total-window-services/static/js/2.87deb777.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/total-window-services/static/js/2.87deb777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6db0023dc28abad21aa",
+    "url": "/total-window-services/static/js/main.291bc98f.chunk.js"
+  },
+  {
+    "revision": "a7fa30370fc45704c09e",
+    "url": "/total-window-services/static/js/runtime-main.ab62e954.js"
+  }
+]);
