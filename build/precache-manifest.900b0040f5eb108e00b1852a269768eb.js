9adb05c20a6a93a04b028e0dbd58fa8ee743b3ae
Harsh Kejriwal <hkejriwal@deloitte.com>
2020-03-14 23:14:41 +0530

    Added build

---
+++ build/precache-manifest.900b0040f5eb108e00b1852a269768eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2795750bbc58bb432b260aef2a9f34f5",
+    "url": "/total-window-services/index.html"
+  },
+  {
+    "revision": "da4c622c57ed59754486",
+    "url": "/total-window-services/static/css/main.25fecd74.chunk.css"
+  },
+  {
+    "revision": "80f9b19f81db589b1e83",
+    "url": "/total-window-services/static/js/2.52324f1e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/total-window-services/static/js/2.52324f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4c622c57ed59754486",
+    "url": "/total-window-services/static/js/main.4e2d6e36.chunk.js"
+  },
+  {
+    "revision": "3192048f84afc2cfb2ec",
+    "url": "/total-window-services/static/js/runtime-main.8bbdfdee.js"
+  },
+  {
+    "revision": "159de8d1aec4e5d9c15b6af6e8f1e0ae",
+    "url": "/total-window-services/static/media/hero-bg.159de8d1.jpg"
+  }
+]);
