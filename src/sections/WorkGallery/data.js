1106c81e5c22b9437b6ec70a26e712b64edabfb1
Harsh Kejriwal <hkejriwal@deloitte.com>
2020-03-17 23:35:43 +0530

    Added remaining images in work gallery

--- src/sections/WorkGallery/data.js
+++ src/sections/WorkGallery/data.js
@@ -9,11 +9,44 @@ import UOM8 from "../../assets/images/UOM/8.jpg";
 import UOM9 from "../../assets/images/UOM/9.jpg";
 import UOM10 from "../../assets/images/UOM/10.jpg";
 import UOM11 from "../../assets/images/UOM/11.jpg";
+
 import AC from "../../assets/images/Abbotsford_Convent/1.jpg";
 
+import BM1 from "../../assets/images/B&M/1.jpg";
+import BM2 from "../../assets/images/B&M/2.jpg";
+import BM3 from "../../assets/images/B&M/3.jpg";
+
+import BRASH1 from "../../assets/images/Brash/1.jpg";
+import BRASH2 from "../../assets/images/Brash/2.jpg";
+
+import GJC1 from "../../assets/images/GJ_Cole/1.jpg";
+import GJC2 from "../../assets/images/GJ_Cole/2.jpg";
+
+import MYER1 from "../../assets/images/Myer/1.jpg";
+import MYER2 from "../../assets/images/Myer/2.jpg";
+import MYER3 from "../../assets/images/Myer/3.jpg";
+import MYER4 from "../../assets/images/Myer/4.jpg";
+
+import PT1 from "../../assets/images/Palais_Theatre/1.jpg";
+import PT2 from "../../assets/images/Palais_Theatre/2.jpg";
+import PT3 from "../../assets/images/Palais_Theatre/3.jpg";
+import PT4 from "../../assets/images/Palais_Theatre/4.jpg";
+
+import SM1 from "../../assets/images/Safety_Matches/1.jpg";
+import SM2 from "../../assets/images/Safety_Matches/2.jpg";
+
+import TRIBECA1 from "../../assets/images/Tribeca/1.jpg";
+import TRIBECA2 from "../../assets/images/Tribeca/2.jpg";
 const workGallery = {
   UOM: [UOM1, UOM2, UOM3, UOM4, UOM5, UOM6, UOM7, UOM8, UOM9, UOM10, UOM11],
-  Abbotsford_Convent: [AC]
+  Abbotsford_Convent: [AC],
+  BM: [BM1, BM2, BM3],
+  Brash: [BRASH1, BRASH2],
+  GJ_Cole: [GJC1, GJC2],
+  Myer: [MYER1, MYER2, MYER3, MYER4],
+  Palais_Theater: [PT1, PT2, PT3, PT4],
+  Safety_Mathes: [SM1, SM2],
+  Tribeca: [TRIBECA1, TRIBECA2] 
 };
 
 export default workGallery;
